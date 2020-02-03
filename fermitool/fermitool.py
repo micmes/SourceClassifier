2a6d1e4d9e03e7254024cfb8265ddc1569908fcf
luana modafferi <luana.modafferi@outlook.com>
2020-02-03 17:57:13 +0100

    added filtered_df attribute

--- fermitool/fermitool.py
+++ fermitool/fermitool.py
@@ -16,9 +16,12 @@ class Fermi_Dataset:
     def __init__(self, handler):
         """
         Constructor. 
-        The argument is the file handler for the FITS file containing the data. 
+        The argument is the file handler for the FITS file containing the data.
+        The handler is an HDUL file and the data is contained in the first extension. 
+        We get rid of the multidimensional columns in order to use pandas.
+        The attribute df is the dataframe originating from the HDUL.
+        The attribute filtered_df is the same dataframe on which some filtering was performed. 
         """
-
         try:
             fits_data = handler[1].data
         except Exception as e:
@@ -29,48 +32,60 @@ class Fermi_Dataset:
         data = t_astropy[col1D].to_pandas()
 
         self._df = data
+        self._filtered_df = self._df.copy(deep=1)
     
     @property
     def df(self):
         return self._df
 
-    def __getitem__(self, df_condition):
+    @property
+    def filtered_df(self):
+        return self._filtered_df
+
+    def filtering(self, df_condition):
         """
         Selects dataframe rows based on df_condition.
+        Note that the filtering operation is always performed over the original data. 
+        So, if you want to filter data on filtered data, you must unite all the conditions
+        and perform the filtering only once.
         """
-        return self._df[df_condition]
+        self._filtered_df = self._df[df_condition]
+        return self
 
     def columns(self):
         """
         Show column names
         """
-        return self._df.columns
+        print(self._df.columns)
 
-    def source_hist(self, colname, df_condition=None, title='Histogram', xlabel='x',
+    def source_hist(self, colname, filter=False, title='Histogram', xlabel='x',
                     ylabel='y', savefig=False, **kwargs):
         """
-        This function provides a histogram plot given a single array in
+        This method provides a histogram plot given a single array in
         input. Most of the features are inherited from the matplotlib hist
         function.
         :colname: The name of the column to plot (str)
-        :df_condition: select dataframe rows
+        :filter: Boolean value. If true, plot data from filtered_df
         :title: the title of the histogram shown in the plot (str)
         :xlabel: x label shown in the plot (str)
         :ylabel: y label shown in the plot (str)
         :savefig: choose whether to save the fig or not
         :kwargs: the same parameters of the plt.hist function (str)
         """
-        if df_condition==None:
-            plt.hist(self.df[colname], **kwargs)
+        if filter==True:
+            plt.hist(self.filtered_df[colname], **kwargs)
         else:
-            plt.hist(self.df[df_condition][colname], **kwargs)
+            plt.hist(self.df[colname], **kwargs)
+        
+
         plt.title(title)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        if savefig:
+        '''if savefig:
             plt.savefig('{}/{}'.format(output_path,title))
         else:
-            plt.show()
+            plt.show()'''
+        plt.savefig(title)
         return
 
     def galactic_map(self):
@@ -98,10 +113,12 @@ if __name__ == '__main__':
     
     #prove
     condition_blazar = data_4FGL.df['CLASS1'].str.match('(bll)|(BLL)')
-    filtered_blazars = data_4FGL[condition_blazar]
-    print(filtered_blazars['CLASS1'])
+    data_4FGL.filtering(condition_blazar)
     
+    data_4FGL.filtering(data_4FGL.df['CLASS1'].str.match('(psr)|(PSR)'))
+    
+    data_4FGL.columns()
+    '''
     data_4FGL.df['Energy_Flux100'] = data_4FGL.df['Energy_Flux100'].multiply(1e12)
     high_latitude_sources = data_4FGL[(abs(data_4FGL.df['GLAT'])>30)]
-    data_4FGL.dist_models()
-    # high_latitude_sources.dist_models()
+    '''
