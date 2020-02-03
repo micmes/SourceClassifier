ef816b948bfde64903837a2b585a1d4d59b26219
Michele <michele.messina.95@gmail.com>
2020-02-03 19:20:20 +0100

    improved control on source_hist

--- tests/test_sourceclassifier.py
+++ tests/test_sourceclassifier.py
@@ -1,16 +1,33 @@
 import unittest
 
 # make sure a __init__.py file exist in the import folder
-from sourceclassifier.sourcehist import sourcehist
+# requires setup.sh to run
+from fermitool.fermitool import *
 
-class testSourceClassifier(unittest.TestCase):
+# requires setup.sh to run
+source_root = os.environ["SOURCE_ROOT"]
+data_path = source_root + '/data/gll_psc_v21.fit'
+
+try:
+    with fits.open(data_path) as hdul:
+        TEST_DF = Fermi_Dataset(hdul)
+except OSError as e:
+    print(e)
+
+class testFermiTool(unittest.TestCase):
 
     def test_sourcehist(self):
         """
         Test the sourcehist function"""
-        x = [1,2,3,4,5,4,2,3,4,2,1,5,6]
+        print(TEST_DF.df.columns)
+        TEST_DF.source_hist('RAJ2000', title='Ra(J2000)',
+                           xlabel='bins', ylabel="occurrences")
 
-        sourcehist(x, title='plt hist test', xlabel='values', ylabel='occurrences')
+        # try filtering
+        TEST_DF.filtering(abs(TEST_DF.df['GLAT'])>30)
+        TEST_DF.source_hist('RAJ2000', filter=True,
+                            title='RA(J2000) with condition',
+                            xlabel='bins', ylabel='occurrences')
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
